8071fe53a8239362d65206d2af34cd1b57cdd435
Rahul S. Shinde <rshinde@mica.edu>
2019-05-26 23:56:50 -0400

    new layouts

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
-  $('body').on('click', openGate);
+  $('#homepage').on('click', openGate);
+  $('body').addClass('visible');
+  $('.nav_link').on('click', followLink)
 });
 
 openGate = function(){
   $('body').addClass('open_gate');
+  setTimeout(function(){
+    $('body').addClass('fade_out');
+  }, 600);
+  setTimeout(function(){
+    window.location.href = 'store.html';
+  }, 1400);
+}
+
+followLink = function(e){
+  var link = $(e.target).attr('href');
+  console.log(link);
+  e.preventDefault();
+  e.stopPropagation();
+  $('body').removeClass('visible');
+  setTimeout(function(){
+    window.location.href = link;
+  }, 600); 
 }
